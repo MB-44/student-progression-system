d98f3566c97ad0bb2cb23e2e1c6828499d2c05f7
Menath <93506278+Lakvindubaddegama@users.noreply.github.com>
2023-11-19 22:56:10 +0530

    Updated | 11/19 | 10.56PM

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    # label = Text(Point(20,125), "Number of Outcomes")
+    # label.draw(window)
--- w2052371_Part01.py
+++ w2052371_Part01.py
@@ -4,6 +4,8 @@
 # Student ID: 20234004
 # Date: 07/11/2023
 
+from graphics import *
+
 print(" **** STUDENT PROGRESSION SOFTWARE ****")
 
 eachOutcomesCount = {"Progress": 0,"Trailer": 0,"Retriever": 0,"Exclude": 0}
@@ -43,8 +45,75 @@ def progressionOutcome(eachOutcomesCount):
                 eachOutcomesCount["Retriever"] += 1
                 return "Do not progress - Module retriever"
 
+def drawBar(window,x,y,width,height,color):
+    bar = Rectangle(Point(x,y), Point(x+width,y-height))
+    bar.setFill(color)
+    bar.draw(window)
+
+def histogram(eachOutcomesCount):
+    values = list(eachOutcomesCount.values())
+    colors = ["#7FCCE5", "#5DA4D9", "#3B7ABD", "#235D9F"]
+
+    maxValue =max(values)
+    windowWidth, windowHeight = 600, max(500, maxValue*10+100)
+    window = GraphWin("Histogram",windowWidth,windowHeight)
+    barWidth, spacing, x = 80,30,50
+
+    totalMessage = Text(Point(windowWidth/2,windowHeight-30),f"{sum(values)} Total Outcomes!")
+    totalMessage.setSize(14)
+    totalMessage.draw(window)
+
+    title = Text(Point(windowWidth/2,30),"Histogram Result")
+    title.setSize(16), title.setStyle("bold")
+    title.draw(window)
+    
+    for i,value in enumerate(values):
+        drawBar(window, x, windowHeight-80, barWidth, value*10,colors[i])
+        x += barWidth + spacing
+
+    yTicks = [tick for tick in range(0,max(values)+10,10)]
+    for yTick in yTicks:
+        label = Text(Point(30,windowHeight-80-yTick*10), str(yTick))
+        label.setSize(10)
+        label.draw(window)
+    
+    xLabels = list(eachOutcomesCount.keys())
+    availableWidth = windowWidth - 2*barWidth
+    xLabelPositions = [
+        barWidth + (i*availableWidth)/3 for i in range(4)
+    ]
+
+    for index, labelText in enumerate(xLabels):
+        xLabel = Text(Point(xLabelPositions[i],windowHeight-60),labelText)
+        xLabel.setSize(12), xLabel.setStyle("bold"), xLabel.setTextColor("#3b393a")
+        xLabel.draw(window)
+    
+    yAxisLine = Line(Point(47,windowHeight-81), Point(windowWidth-120,windowHeight-81))
+    yAxisLine.draw(window)
+    
+    # xLabel = Text(Point(50 + barWidth / 2, 370), "Progress")
+    # xLabel.draw(window)
+
+    # xLabel = Text(Point(50 + barWidth + spacing + barWidth / 2, 370), "Trailer")
+    # xLabel.draw(window)
+
+    # xLabel = Text(Point(50 + 2 * (barWidth + spacing) + barWidth / 2, 370), "Retriever")
+    # xLabel.draw(window)
+
+    # xLabel = Text(Point(50 + 3 * (barWidth + spacing) + barWidth / 2, 370), "Excluded")
+    # xLabel.draw(window)
+
+    # yAxisLine = Line(Point(47, 351), Point(380,351))
+    # yAxisLine.draw(window)
+
+    window.getMouse()
+    window.close()
+
 while True:
     print(progressionOutcome(eachOutcomesCount))
-    userChoice = str(input("\nWould you like to enter another set of data ?\nEnter 'y' for Yes or 'q' to Quit and view results: ")).title()
-    if userChoice != "Y":
+    userChoice = str(input("\nWould you like to enter another set of data ?\nEnter for Yes or 'q' to Quit and view results: ")).title()
+    if userChoice == "Q":
         break
+
+histogram(eachOutcomesCount)
+
