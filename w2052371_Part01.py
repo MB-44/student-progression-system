cdde39e5e8f75c380391571c66aa22c1a597145b
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-11-20 22:25:17 +0530

    Fixed | Almost there | 11/20 | 10.25pm

--- w2052371_Part01.py
+++ w2052371_Part01.py
@@ -8,8 +8,6 @@ from graphics import *
 
 print(" **** STUDENT PROGRESSION SOFTWARE ****")
 
-eachOutcomesCount = {"Progress": 0,"Trailer": 0,"Retriever": 0,"Exclude": 0}
-
 def progressionOutcome(eachOutcomesCount):
         while True:
             try:
@@ -44,8 +42,9 @@ def progressionOutcome(eachOutcomesCount):
                 eachOutcomesCount["Retriever"] += 1
                 return "Do not progress - Module retriever"
 
-def drawBar(window,x,y,width,height,color):
-    bar = Rectangle(Point(x,y), Point(x+width,y-height))
+
+def drawBar(window,x,y,barWidth,barHeight,color):
+    bar = Rectangle(Point(x,y), Point(x+barWidth,y-barHeight))
     bar.setFill(color)
     bar.draw(window)
 
@@ -54,42 +53,48 @@ def histogram(eachOutcomesCount):
     colors = ["#7FCCE5", "#5DA4D9", "#3B7ABD", "#235D9F"]
 
     maxValue =max(values)
-    windowWidth, windowHeight = 600, max(500, maxValue*10+100)
-    window = GraphWin("Histogram",windowWidth,windowHeight)
+    windowWidth, windowHeight = 600, max(450, maxValue*10+100)
     barWidth, spacing, x = 80,30,50
 
-    totalMessage = Text(Point(windowWidth/2,windowHeight-30),f"{sum(values)} Total Outcomes!")
-    totalMessage.setSize(14)
-    totalMessage.draw(window)
+    window = GraphWin("Histogram",windowWidth,windowHeight)
+    
+    xLabels = list(eachOutcomesCount.keys())
+    availableWidth = windowWidth - 2 * barWidth
+    xLabelPositions = [barWidth + (i * availableWidth) / 3 for i in range(4)]
 
-    title = Text(Point(windowWidth/2,30),"Histogram Result")
+    title = Text(Point(120, 30),"Histogram Result")
     title.setSize(16), title.setStyle("bold")
     title.draw(window)
     
-    for i,value in enumerate(values):
-        drawBar(window, x, windowHeight-80, barWidth, value*10,colors[i])
-        x += barWidth + spacing
+    totalMessage = Text(Point(windowWidth / 2, windowHeight - 30), f"{sum(values)} Total Outcomes!")
+    totalMessage.setSize(14)
+    totalMessage.draw(window)
 
-    yTicks = [tick for tick in range(0,max(values)+10,10)]
+    # for i,value in enumerate(values):
+    #     drawBar(window, x, windowHeight-80, barWidth, value*10,colors[i])
+    #     x += barWidth + spacing
+    
+    for i, value in enumerate(values):
+        drawBar(window, xLabelPositions[i], windowHeight - 80, barWidth, value * 10, colors[i])
+
+    yTicks = [tick for tick in range(0, max(values) + 10, 10)]
     for yTick in yTicks:
-        label = Text(Point(30,windowHeight-80-yTick*10), str(yTick))
+        label = Text(Point(30, windowHeight - 80 - yTick * 10), str(yTick))
         label.setSize(10)
         label.draw(window)
-    
-    xLabels = list(eachOutcomesCount.keys())
-    availableWidth = windowWidth - 2*barWidth
-    xLabelPositions = [barWidth + (i*availableWidth)/3 for i in range(4)]
-    
+
     for index, labelText in enumerate(xLabels):
-        xLabel = Text(Point(xLabelPositions[index],windowHeight-60),labelText)
+        xLabel = Text(Point(xLabelPositions[index], windowHeight - 60), labelText)
         xLabel.setSize(12), xLabel.setStyle("bold"), xLabel.setTextColor("#3b393a")
         xLabel.draw(window)
     
-    yAxisLine = Line(Point(47,windowHeight-81), Point(windowWidth-120,windowHeight-81))
+    yAxisLine = Line(Point(xLabelPositions[0], windowHeight - 81), Point(xLabelPositions[-1] + barWidth, windowHeight - 81))
     yAxisLine.draw(window)
 
     window.getMouse()
     window.close()
+
+eachOutcomesCount = {"Progress": 0,"Trailer": 0,"Retriever": 0,"Exclude": 0}
 
 while True:
     print(progressionOutcome(eachOutcomesCount))
