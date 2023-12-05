05cf94f342a40ae35dc761b3d5d7af16460b28aa
Menath-Baddegama <93506278+Menath-Baddegama@users.noreply.github.com>
2023-12-05 20:58:58 +0530

    --

--- test_file.py
+++ test_file.py
@@ -1,4 +1,15 @@
-def verions():
-    return "hello"
-if verions() == "hello":
-    print(";;;;")
+print("student progression software")
+
+def check_input():
+    while True:
+        try:
+            pass_credit = int(input("Enter your credit at pass: "))
+            defer_credit = int(input("Enter your credit at defer: "))
+            fail_credit = int(input("Enter your credit at fail: "))
+
+            for credit in [pass_credit,defer_credit,fail_credit]:
+                
+
+        except ValueError:
+            print("Integer required!")
+            continue
